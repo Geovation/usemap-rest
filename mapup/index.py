fec6ea5c7ad68a31c9f6f9c7193b2538baa80228
lewisj <lewisj@users.noreply.github.com>
2022-06-08 11:24:46 +0100

    kinda working auto complete - ToDo error parsing

--- mapup/index.py
+++ mapup/index.py
@@ -1,7 +1,29 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+import requests
+import urllib.parse
+
+load_dotenv()
+
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/autofill/<input>")
+
+def free_test_places(input):
+    api_url = "https://api.os.uk/search/places/v1/find?"
+    params = {"query":input, 'key': 'YOlPBwkiXXOsZiEPuB076sDjYTIOJqfG','maxresults': '30'}
+    # print(api_url + urllib.parse.urlencode(params))
+    response = requests.get(api_url + urllib.parse.urlencode(params))
+    json = response.json()['results']
+    fresh_address = {}
+
+    for value in json:
+        fresh_address[value['DPA']['UPRN']] = value['DPA']
+    return fresh_address
+
+
 def hello_world():
     return "Hello, World!"
