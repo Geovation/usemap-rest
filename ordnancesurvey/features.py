f249ce2beb1208d0d88ff77ff09b215ae161c7ed
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2022-11-08 16:11:02 +0000

    add feature call and second places call

---
+++ ordnancesurvey/features.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import urllib.parse
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_feature_from_toid(toid):
+    """Get feature from given TOID"""
+    xml = '<ogc:Filter>'
+    xml += '<ogc:PropertyIsEqualTo>'
+    xml += '<ogc:PropertyName>TOID</ogc:PropertyName>'
+    xml += '<ogc:Literal>' + toid + '</ogc:Literal>'
+    xml += '</ogc:PropertyIsEqualTo>'
+    xml += '</ogc:Filter>'
+
+    wfsParams = {
+        'key': os.environ.get("os_api_key"),
+        'service': 'WFS',
+        "request": 'GetFeature',
+        "version": '2.0.0',
+        "typeNames": 'Topography_TopographicArea',
+        "propertyName": 'TOID,DescriptiveGroup,SHAPE,Theme,ChangeDate,ChangeHistory,CALCULATEDAREAVALUE',
+        "outputFormat": 'GEOJSON',
+        "srsName": 'urn:ogc:def:crs:EPSG::4326',
+        "filter": xml,
+        "count": 1
+    }
+
+    api_url = "https://api.os.uk/features/v1/wfs?"
+   
+    response = requests.get(api_url + urllib.parse.urlencode(wfsParams))
+    json = response.json()
+
+    return json
--- ordnancesurvey/places.py
+++ ordnancesurvey/places.py
@@ -5,7 +5,6 @@ import urllib.parse
 from dotenv import load_dotenv
 load_dotenv()
 
-
 def get_places_from_search(search):
     """Get places results from search term"""
 
@@ -13,10 +12,35 @@ def get_places_from_search(search):
     params = {"query": search, 'key': os.environ.get(
         "os_api_key"), 'maxresults': '30'}
     response = requests.get(api_url + urllib.parse.urlencode(params))
-    json = response.json()['results']
+    json = response.json()['results'] if 'results' in response.json() else response.json()
 
     fresh_addresses = []
 
     for value in json:
         fresh_addresses.append(value['DPA'])
     return fresh_addresses
+
+def get_building_from_toid(toid):
+    """Get building results from TOID term thru UPRN"""
+
+    uprn = 0
+    api_url_1 = f"https://api.os.uk/search/links/v1/identifierTypes/TOID/{toid}?"
+    params = {'key': os.environ.get("os_api_key")}
+    response = requests.get(api_url_1 + urllib.parse.urlencode(params))
+    json_uprn = response.json()
+    if 'correlations' in json_uprn: 
+        uprn = int(json_uprn['correlations'][0]['correlatedIdentifiers'][0]['identifier'])
+    else:
+        uprn = 0
+    if uprn > 0:
+        api_url_2  = "https://api.os.uk/search/places/v1/uprn?"
+        params = {"uprn": uprn, 'key': os.environ.get("os_api_key")}
+        response = requests.get(api_url_2 + urllib.parse.urlencode(params))
+        json = response.json()['results'] if 'results' in response.json() else response.json()
+        fresh_addresses = []
+        for value in json:
+            fresh_addresses.append(value['DPA'])
+        return fresh_addresses
+    else:
+        json = [{'message': 'no uprn found'}]
+    return json
