ec38247ba15f3c24d3ff33ef8c38766f449891ed
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2022-11-09 13:36:24 +0000

    nomenclature + return statement fix in places.py

--- ordnancesurvey/places.py
+++ ordnancesurvey/places.py
@@ -12,32 +12,31 @@ def get_places_from_search(search):
     params = {"query": search, 'key': os.environ.get(
         "os_api_key"), 'maxresults': '30'}
     response = requests.get(api_url + urllib.parse.urlencode(params))
-    json = response.json()['results'] if 'results' in response.json() else response.json()
 
     fresh_addresses = []
-
-    for value in json:
-        fresh_addresses.append(value['DPA'])
+    if 'results' in response.json():
+        json = response.json()['results']
+        for value in json:
+            fresh_addresses.append(value['DPA'])
+            
     return fresh_addresses
 
 def get_building_from_toid(toid):
     """Get building results from TOID term thru UPRN"""
-
-    uprn = 0
-    api_url_1 = f"https://api.os.uk/search/links/v1/identifierTypes/TOID/{toid}?"
+    linked_ident_api_url = f"https://api.os.uk/search/links/v1/identifierTypes/TOID/{toid}?"
     params = {'key': os.environ.get("os_api_key")}
-    response = requests.get(api_url_1 + urllib.parse.urlencode(params))
+    response = requests.get(linked_ident_api_url + urllib.parse.urlencode(params))
     json_uprn = response.json()
     if 'correlations' in json_uprn: 
         uprn = int(json_uprn['correlations'][0]['correlatedIdentifiers'][0]['identifier'])
-        api_url_2  = "https://api.os.uk/search/places/v1/uprn?"
+        places_api_url  = "https://api.os.uk/search/places/v1/uprn?"
         params = {"uprn": uprn, 'key': os.environ.get("os_api_key")}
-        response = requests.get(api_url_2 + urllib.parse.urlencode(params))
+        response = requests.get(places_api_url + urllib.parse.urlencode(params))
+
         fresh_addresses = []
         if 'results' in response.json():
             json = response.json()['results']
             for value in json:
                 fresh_addresses.append(value['DPA'])
-            return fresh_addresses
-    else:
-        return []
+
+        return fresh_addresses
