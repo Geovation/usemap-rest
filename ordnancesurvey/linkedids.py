5eeab28fb47c2b8960be6fea21d16724be3d3635
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2022-11-22 13:34:02 +0000

    added linkedids endpoint

--- ordnancesurvey/linkedids.py
+++ ordnancesurvey/linkedids.py
@@ -9,10 +9,9 @@ def get_linked_ids_from_uprn(uprn):
     """Get linked identifiers from a given UPRN"""
     
     json = {}
-    api_url = f"https://api.os.uk/search/links/v1/identifierTypes/UPRN/{uprn}"
+    api_url = f"https://api.os.uk/search/links/v1/identifierTypes/UPRN/{uprn}?"
     params = {'key': os.environ.get("os_api_key")}
     response = requests.get(api_url + urllib.parse.urlencode(params))
     json = response.json()
-    print(response.json())
     
     return json
